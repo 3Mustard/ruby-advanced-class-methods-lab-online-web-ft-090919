d1d6c85e7e0a994fcd1646aa0f52251894905782
Matthew C <m23.cullen@gmail.com>
2019-09-20 01:19:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -61,7 +61,10 @@ class Song
   
   def self.create_from_filename(name)
     new_file = self.new_file(name)
-    song = self.
+    song = self.create
+    song.name = new_file.name 
+    song.artist_name = new_file.artist_name
+    song
   end 
 end
 
