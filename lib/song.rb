8256105b6974ca6bedd5cfb99272ab0a26209419
Matthew C <m23.cullen@gmail.com>
2019-09-20 00:52:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,6 +37,10 @@ class Song
   
   def self.find_or_create_by_name(name)
     result = self.find_by_name(name)
+    if result
+      result
+    else 
+      self.create_by_name(name)
   end 
 end
 
